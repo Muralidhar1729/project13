c6ad5b0cddcee622ae0f69d06931ff6b8b5dd20e
muralidhar <muralidharkurudi1729@gmail.com>
2023-12-09 13:17:23 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+def srujana(request):
+    return render(request,'srujana.html')
+def sudeer(request):
+    return render(request,'sudeer.html')
+def srujana2(request):
+    return render(request,'srujana2.html')
+def sudeer2(request):
+    return render(request,'sudeer2.html')
+
+
+# Create your views here.
